cdefb2ea14110103423c66b6e04fa720e7029c03
lyyljs <lyyljs@gmail.com>
2016-07-30 15:10:34 +0800

    add markdown support

--- admin.py
+++ admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from pagedown.widgets import AdminPagedownWidget
+from django import forms
 
 class UserAdmin(admin.ModelAdmin):
 	fields = ['user_name', 'user_password', 'user_phone', 'user_email', 'create_date', 'update_date']
@@ -13,9 +15,18 @@ class TagAdmin(admin.ModelAdmin):
 	fields = ['tag_name']
 	search_fields = ['tag_name']
 
+class ArticleForm(forms.ModelForm):
+	content = forms.CharField(widget=AdminPagedownWidget())
+
+	class Meta:
+		model = Article
+		fields = '__all__'
+
+class ArticleAdmin(admin.ModelAdmin):
+	form = ArticleForm
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment)
